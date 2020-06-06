8e6a9487823d73039333b718e83ee811cb54f456
tramvay5401 <47867693+tramvay5401@users.noreply.github.com>
2020-06-06 10:11:41 +0300

    Framework start (#3)
    * Finish base frawemork
    
    * Comment debug

---
+++ src/core/DomListener.js
@@ -0,0 +1,34 @@
+import {capitalize} from "@core/utils";
+
+export class DomListener {
+    constructor($root, listeners = []) {
+        if (!$root) {
+            throw new Error(`NO $root provided for DomListener!`)
+        } else {
+            this.$root = $root
+            this.listeners = listeners
+        }
+    }
+
+    innitDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            if (!this[method]) {
+                throw new Error(`Method${method} is not implemented in ${this.name || ''} component`)
+            }
+            this[method] = this[method].bind(this)
+            this.$root.on(listener, this[method])
+        })
+    }
+
+    removeDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            this.$root.off(listener, this[method])
+        })
+    }
+}
+
+function getMethodName(eventName) {
+    return 'on' + capitalize(eventName)
+}
